e8ade388bcaf539e520155f9afea01bcc09509cf
Mansoury <mansoury@caspian.com>
2018-10-02 14:35:39 +0330

    SourceMaps for Js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,16 @@ var gulp = require('gulp'),
 
 var sourceCss = ['src/css/agency.css','src/style.css'];
 
+var sourceJs = ['src/js/agency.min.js','src/js/jqBootstrapValidation.js','src/js/contact_me.js'];
 //****************************** */
+
+gulp.task('concat-js',function(){
+    return gulp.src(sourceJs)
+           .pipe(sourceMaps.init())
+           .pipe(concat('scripts.js',{newLine:'\n \n \n /*!-------------------Next File--------------*/ \n \n'}))
+           .pipe(sourceMaps.write('../maps'))
+           .pipe(gulp.dest('dist/js'))
+});
 
 gulp.task('concat-css',function(){
     return gulp.src(sourceCss)
