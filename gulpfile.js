3dfdfac949df27353ade6ebed0f9fd171ce82d1a
Mansoury <mansoury@caspian.com>
2018-10-02 15:30:23 +0330

    minifyCss and rename css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
 //gulpfile.js
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    sourceMaps = require('gulp-sourcemaps');
+    sourceMaps = require('gulp-sourcemaps'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename');
+
 
 var sourceCss = ['src/css/agency.css','src/style.css'];
 
 var sourceJs = ['src/js/agency.min.js','src/js/jqBootstrapValidation.js','src/js/contact_me.js'];
 //****************************** */
+
+
+
+
+gulp.task('hello',function(){
+    console.log("hello Aso");
+});
+
+gulp.task('concat-css',function(){
+    return gulp.src(sourceCss)
+    .pipe(sourceMaps.init())
+    .pipe(concat('mystyles.css',{newLine:'\n \n \n /*!-------------------Next File--------------*/ \n \n'}))
+    .pipe(sourceMaps.write("../maps"))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(cssmin({level:2}))
+    .pipe(rename({suffix:'.min'}))
+    .pipe(gulp.dest('dist/css'))
+});
 
 gulp.task('concat-js',function(){
     return gulp.src(sourceJs)
@@ -16,15 +37,10 @@ gulp.task('concat-js',function(){
            .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('concat-css',function(){
-    return gulp.src(sourceCss)
-    .pipe(sourceMaps.init())
-    .pipe(concat('mystyles.css',{newLine:'\n \n \n /*!-------------------Next File--------------*/ \n \n'}))
-    .pipe(sourceMaps.write("../maps"))
-    .pipe(gulp.dest('dist/css'));
-});
 
-gulp.task('hello',function(){
-    console.log("hello Aso");
+gulp.task('minifyCss',function(){
+    return gulp.src('dist/css/*.css')
+           .pipe(cssmin({level:2}))
+           .pipe(rename({suffix:'.min'}))
+           .pipe(gulp.dest('dist/css'))
 });
-
